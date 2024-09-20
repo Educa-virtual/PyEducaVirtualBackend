8808eb531018589544355998a6025e2a36d8f444
Aron <blazte.aster@gmail.com>
2024-09-20 11:59:08 -0500

    feat: agrega asignar matriz para las preguntas

---
+++ app/Http/Controllers/Ere/BancoPreguntasController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers\Ere;
+
+use App\Http\Controllers\ApiController;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BancoPreguntasController extends ApiController
+{
+
+    public function guardarPreguntaConAlternativas(Request $request)
+    {
+        $datosPreguntaJson = json_encode($request->datosPregunta);
+        $datosAlternativasJson = json_encode($request->datosAlternativas);
+        $params = [
+            'ere',
+            'preguntas',
+            $datosPreguntaJson,
+            'alternativas',
+            $datosAlternativasJson,
+            'iPreguntaId'
+        ];
+
+        try {
+
+            $resp = DB::statement(
+                'EXEC grl.SP_INS_EnTablaMaestroDetalleDesdeJSON 
+                    @Esquema = ?,
+                    @TablaMaestra = ?,
+                    @DatosJSONMaestro = ?,
+                    @TablaDetalle = ?,
+                    @DatosJSONDetalles = ?,
+                    @campoFK = ?
+                ',
+                $params
+            );
+
+            return $this->successResponse(
+                $resp,
+                'Datos guardados correctamente'
+            );
+        } catch (Exception $e) {
+            return $this->errorResponse($e, 'Error al obtener los datos');
+        }
+    }
+    public function actualizarMatrizPreguntas(Request $request)
+    {
+
+        $preguntas = $request->preguntas;
+
+        if (!is_array($preguntas)) {
+            return $this->errorResponse(null, 'Datos mal formateados');
+        }
+
+        try {
+            foreach ($preguntas as $pregunta) {
+                $datosJson = json_encode($pregunta['datosJson']);
+
+                $condiciones = [
+                    [
+                        'COLUMN_NAME' => "iPreguntaId",
+                        'VALUE' => $pregunta['iPreguntaId']
+                    ]
+                ];
+                $condicionesJson = json_encode($condiciones);
+
+                $params = [
+                    'ere',
+                    'preguntas',
+                    $datosJson,
+                    $condicionesJson
+                ];
+                $resp = DB::statement(
+                    'EXEC grl.SP_UPD_EnTablaConJSON
+                        @Esquema = ?,
+                        @Tabla = ?,
+                        @DatosJSON = ?,
+                        @CondicionesJSON = ?
+                    ',
+                    $params
+                );
+            }
+
+            return $this->successResponse(
+                $resp,
+                'Datos guardados correctamente'
+            );
+        } catch (Exception $e) {
+            return $this->errorResponse($e, 'Error al actualizar los datos');
+        }
+    }
+
+    public function obtenerBancoPreguntas(Request $request)
+    {
+
+        $campos = 'iPreguntaId,iCompentenciaId,iCapacidadId,iDesempenoId,cPregunta,cPreguntaTextoAyuda,iPreguntaNivel,iPreguntaPeso,dtPreguntaTiempo,bPreguntaEstado,cPreguntaClave';
+
+        $bPreguntaEstado = $request->bPreguntaEstado ?? 0;
+        $where = "bPreguntaEstado = {$bPreguntaEstado}";
+
+        $params = [
+            'ere',
+            'preguntas',
+            $campos,
+            $where
+        ];
+
+
+        try {
+            $preguntas = DB::select('EXEC grl.sp_SEL_DesdeTabla_Where 
+                @nombreEsquema = ?,
+                @nombreTabla = ?,    
+                @campos = ?,        
+                @condicionWhere = ?
+            ', $params);
+            return $this->successResponse(
+                $preguntas,
+                'Datos obtenidos correctamente'
+            );
+        } catch (Exception $e) {
+            return $this->errorResponse($e, 'Error al obtener los datos');
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,25 @@
 <?php
 
 use App\Http\Controllers\CredencialController;
+use App\Http\Controllers\Ere\BancoPreguntasController;
+use App\Http\Controllers\Ere\CompetenciasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-Route::post('/login',[CredencialController::class,'login']);
+Route::post('/login', [CredencialController::class, 'login']);
+
+
+Route::group(['prefix' => 'ere'], function () {
+    Route::group(['prefix' => 'competencias'], function () {
+        Route::get('getCompetenciasByAreaId', [CompetenciasController::class, 'getCompetenciasByAreaId']);
+    });
+
+    Route::group(['prefix' => 'banco-preguntas'], function () {
+        Route::post('guardarPreguntaConAlternativas', [BancoPreguntasController::class, 'guardarPreguntaConAlternativas']);
+        Route::patch('actualizarMatrizPreguntas', [BancoPreguntasController::class, 'actualizarMatrizPreguntas']);
+        Route::get('obtenerBancoPreguntas', [BancoPreguntasController::class, 'obtenerBancoPreguntas']);
+    });
+});
