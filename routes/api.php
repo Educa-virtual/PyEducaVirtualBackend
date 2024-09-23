14f70a8892b0a597bbee79661f880e4d7b4e13c3
Mitwar99 <mitwarhuacan@hotmail.com>
2024-09-23 15:24:34 -0500

    Mostrar curso

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CredencialController;
 use App\Http\Controllers\Ere\BancoPreguntasController;
 use App\Http\Controllers\Ere\CompetenciasController;
+use App\Http\Controllers\Ere\cursoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,13 +14,18 @@ Route::post('/login', [CredencialController::class, 'login']);
 
 
 Route::group(['prefix' => 'ere'], function () {
-    Route::group(['prefix' => 'competencias'], function () {
+    
+    /*Route::group(['prefix' => 'competencias'], function () {
         Route::get('getCompetenciasByAreaId', [CompetenciasController::class, 'getCompetenciasByAreaId']);
-    });
+    });*/
 
     Route::group(['prefix' => 'banco-preguntas'], function () {
         Route::post('guardarPreguntaConAlternativas', [BancoPreguntasController::class, 'guardarPreguntaConAlternativas']);
         Route::patch('actualizarMatrizPreguntas', [BancoPreguntasController::class, 'actualizarMatrizPreguntas']);
         Route::get('obtenerBancoPreguntas', [BancoPreguntasController::class, 'obtenerBancoPreguntas']);
+    });
+    
+    Route::group(['prefix' => 'curso'], function () {
+        Route::get('obtenerCursos', [cursoController::class, 'obtenerCursos']);
     });
 });
