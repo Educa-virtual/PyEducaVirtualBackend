43b2b61a84199b71940dccc87f4acbc1349e7507
carlos alberto chura flores <chura.f5@gmail.com>
2024-09-25 11:31:26 -0500

    Se realizo acciones en los siguientes archivos: name.blade.php => se modigico su interfaz de envio de codigo

--- app/Http/Controllers/api/seg/sel/ListarCursosController.php
+++ app/Http/Controllers/api/seg/sel/ListarCursosController.php
@@ -17,8 +17,6 @@ class ListarCursosController extends Controller
                             acur.iCursoId,
                             acur.iCurrId,
                             acur.iTipoCursoId,
-                            --acur.iGradoId,
-                            --acur.iNivelId,
                             acur.cCursoNombre,
                             acur.nCursoCredTeoria,
                             acur.nCursoCredPractica,
