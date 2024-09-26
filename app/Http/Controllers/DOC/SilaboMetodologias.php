2eb98b22a74458b7b5061fbaa76ecc0579ab6a96
DREW <jhoand60@gmail.com>
2024-09-26 10:57:13 -0500

    feat: SilaboMetodologias - TipoMetodologias - Silabos - update routes

---
+++ app/Http/Controllers/DOC/SilaboMetodologias.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\DOC;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SilaboMetodologias extends Controller
+{
+    //
+}
--- routes/docente.php
+++ routes/docente.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\DOC\Curriculas;
 use App\Http\Controllers\DOC\Cursos;
 use App\Http\Controllers\DOC\DocenteCursos;
+use App\Http\Controllers\DOC\SilaboMetodologias;
+use App\Http\Controllers\DOC\Silabos;
+use App\Http\Controllers\DOC\TipoMetodologias;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'docente'], function () {
@@ -14,5 +17,14 @@ Route::group(['prefix' => 'docente'], function () {
   });
   Route::group(['prefix' => 'cursos'], function () {
     Route::post('list', [Cursos::class, 'list']);
+  });
+  Route::group(['prefix' => 'silabos'], function () {
+    Route::post('list', [Silabos::class, 'list']);
+  });
+  Route::group(['prefix' => 'tipo-metodologias'], function () {
+    Route::post('list', [TipoMetodologias::class, 'list']);
+  });
+  Route::group(['prefix' => 'silabo-metodologias'], function () {
+    Route::post('list', [SilaboMetodologias::class, 'list']);
   });
 });
