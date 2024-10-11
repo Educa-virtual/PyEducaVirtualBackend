972458d2651ac27b70e7226495d50e682f4f6d75
Aron <blazte.aster@gmail.com>
2024-10-11 15:36:22 -0500

    Evaluaciones (#8)

--- routes/evaluaciones/api.php
+++ routes/evaluaciones/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Evaluaciones\BancoPreguntasController;
+use App\Http\Controllers\Evaluaciones\EvaluacionController;
+use App\Http\Controllers\Evaluaciones\TipoEvaluacionController as EvaluacionesTipoEvaluacionController;
 use App\Http\Controllers\Evaluaciones\TipoPreguntaController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,10 +11,16 @@ Route::group(['prefix' => 'evaluaciones',], function () {
         Route::get('obtenerTipoPreguntas', [TipoPreguntaController::class, 'obtenerTipoPreguntas']);
     });
 
+    Route::resource('tipo-evaluaciones', EvaluacionesTipoEvaluacionController::class);
+
     Route::group(['prefix' => 'banco-preguntas'], function () {
         Route::post('guardarActualizarPreguntaConAlternativas', [BancoPreguntasController::class, 'guardarActualizarPreguntaConAlternativas']);
         Route::get('obtenerBancoPreguntas', [BancoPreguntasController::class, 'obtenerBancoPreguntas']);
         Route::get('obtenerEncabezadosPreguntas', [BancoPreguntasController::class, 'obtenerEncabezadosPreguntas']);
         Route::delete('eliminarBancoPreguntasById/{id}', [BancoPreguntasController::class, 'eliminarBancoPreguntasById']);
+    });
+
+    Route::group(['prefix' => 'evaluacion'], function () {
+        Route::post('guardarActualizarEvaluacion', [EvaluacionController::class, 'guardarActualizarEvaluacion']);
     });
 });
