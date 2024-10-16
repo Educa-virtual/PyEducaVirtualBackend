953e1f1220aa87290291a7d7f35da029e4ec0466
Aron <blazte.aster@gmail.com>
2024-10-16 11:42:14 -0500

    refactor: cambia banco preguntas encabezado repository

--- routes/aula/api.php
+++ routes/aula/api.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\aula\AulaVirtualController;
 use Illuminate\Support\Facades\Route;
+
 Route::group(['prefix' => 'aula-virtual'], function () {
     Route::group(['prefix' => 'contenidos'], function () {
         Route::group(['prefix' => 'actividad'], function () {
             Route::post('guardarActividad', [AulaVirtualController::class, 'guardarActividad']);
+            Route::delete('eliminarActividad', [AulaVirtualController::class, 'eliminarActividad']);
         });
 
         Route::get('contenidoSemanasProgramacionActividades', [AulaVirtualController::class, 'contenidoSemanasProgramacionActividades']);
