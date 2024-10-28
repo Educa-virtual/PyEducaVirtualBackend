42699d8385dfef387d1556c8e83a6aa1d7282478
carlos alberto chura flores <chura.f5@gmail.com>
2024-10-28 15:37:30 -0500

    Se Agregaron las siguientes funciones: AsistenciaController.php => Se agrego la funcion de reporte para asistencia asistencia_reporte_mensual.blade.php => Es la plantilla de reporte mensual

--- app/Http/Controllers/api/asi/AsistenciaController.php
+++ app/Http/Controllers/api/asi/AsistenciaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api\asi;
 
 use App\Http\Controllers\Controller;
+use DateTime;
 use Exception;
 use Hashids\Hashids;
 use Illuminate\Http\JsonResponse;
@@ -49,5 +50,92 @@ class AsistenciaController extends Controller
         }
 
         return new JsonResponse($response,$estado);
+    }
+    public function report(){
+       
+        $primera_fecha = new DateTime();
+        $primerDia = $primera_fecha->modify("first day of this month");
+        $ultima_fecha = new DateTime();
+        $ultimoDia = $ultima_fecha->modify("last day of this month");
+
+        $primera_fecha = $primerDia->format('Y-m-d');
+        $tiempo = strtotime($primera_fecha);
+        $ultimo = $ultimoDia->format('d');
+        
+        $nombre_dia = date("l",$tiempo);
+
+        $dia_semana = [
+                    'Sunday' => 'D',
+                    'Monday' => 'L',
+                    'Tuesday' => 'M',
+                    'Wednesday' => 'M',
+                    'Thursday' => 'J',
+                    'Friday' => 'V',
+                    'Saturday' => 'S'
+        ];
+
+        $dia_elegido  = $dia_semana[$nombre_dia];
+        $dias 	  = ['D','L','M','M','J','V','S'];
+        $indice   = array_search($dia_elegido,$dias);
+
+        $principal = array_slice($dias,$indice);
+        $restante = array_slice($dias,0,$indice);
+        
+        $unir_dias = array_merge($principal,$restante);
+
+        $solicitud = [
+            $request->opcion ?? 'REPORTE_MENSUAL',
+            $iCursoId ?? NULL,
+            $request->dtCtrlAsistencia ?? NULL,
+            $request->asistencia_json ?? NULL,
+        ];
+
+        $query=DB::select("execute asi.Sp_CRUD_control_asistencias ?,?,?,?", $solicitud);
+
+        $json_registro = [];
+        
+        for ($i=1; $i <= $ultimo; $i++) {
+            $json_registro [] = ["diaMes"=>strval($i),"cTipoAsiLetra"=>"-"];
+        }
+        
+        foreach ($query as $index => $valor) {
+            $registro = json_decode($valor->diasAsistencia);
+            $paquete = [];
+            foreach($registro as $fila){
+                $paquete[] = $fila->diaMes;
+            }
+            
+            $filtrar = array_filter($json_registro,function($valor) use ($paquete){
+                return !in_array($valor["diaMes"],$paquete);
+            });
+
+            $convertir = json_decode(json_encode($registro),true);
+
+            $unir = array_merge($filtrar,$convertir);
+            
+            usort($unir, function($a,$b){
+                return $a["diaMes"] > $b["diaMes"] ? 1 : -1;
+            });
+
+            $valor->diasAsistencia = $unir;
+            
+        }
+
+        $respuesta = [
+            "ultimodia"=>$ultimo,
+            "query"=>$query,
+            "dias_Semana"=>$unir_dias,
+            "year"=>"2024",
+            "mes"=>"2024-10-01 2024-10-31",
+            "modular"=>"000005600",
+            "dre"=>"DRE MOQUEGUA UGEL",
+            "fecha_reporte"=>"2024-10-01",
+            "fecha_cierre"=>"2024-10-31",
+            "nivel"=>"SECUNDARIO",
+            "periodo"=>"",
+            "grado"=>"",
+        ];
+
+        return view("asistencia_reporte_mensual",$respuesta);
     }
 }
