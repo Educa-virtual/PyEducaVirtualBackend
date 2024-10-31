b63a7e3725122276a41c5314804174537cd4beff
sandoval2024-prog <willyalfredo_123_2020@hotmail.com>
2024-10-31 16:46:26 -0500

    fix: add function list in EscalaCalificacionesController - guardarCalificacionDocente in TareasEstudiantes

--- routes/evaluaciones/api.php
+++ routes/evaluaciones/api.php
@@ -39,4 +39,8 @@ Route::group(['prefix' => 'evaluaciones',], function () {
     });
 
     Route::resource('escala-calificaciones', EscalaCalificacionesController::class);
+
+    Route::group(['prefix' => 'escala-calificaciones'], function () {
+        Route::post('list', [EscalaCalificacionesController::class, 'list']);
+    });
 });
