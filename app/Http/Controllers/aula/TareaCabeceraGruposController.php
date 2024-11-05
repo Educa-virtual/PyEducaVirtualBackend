b4d5f200085f98451c52bd031473d4869c239e2a
DREW <jhoand60@gmail.com>
2024-11-05 14:20:36 -0500

    fix(TareaCabeceraGrupos): add function eliminar y guardar calificacion

--- app/Http/Controllers/aula/TareaCabeceraGruposController.php
+++ app/Http/Controllers/aula/TareaCabeceraGruposController.php
@@ -60,7 +60,7 @@ class TareaCabeceraGruposController extends Controller
                 ?,?,?,?,?,?,?,?,?,?,?,?', $parametros);
 
             foreach ($data as $key => $value) {
-                //$value->iTareaCabGrupoId = $this->hashids->encode($value->iTareaCabGrupoId);
+                $value->iEscalaCalifId = $this->hashids->encode($value->iEscalaCalifId);
                 $value->iTareaId = $this->hashids->encode($value->iTareaId);
             }
 
@@ -126,4 +126,64 @@ class TareaCabeceraGruposController extends Controller
 
         return new JsonResponse($response, $codeResponse);
     }
+
+    public function eliminarTareaCabeceraGrupos(Request $request){
+        
+        $parametros = [
+            $request->iTareaCabGrupoId,
+        ];
+
+        try {
+            $data = DB::select('exec aula.SP_DEL_tareaCabeceraGruposxiTareaCabGrupoId
+                ?', $parametros);
+
+            if ($data[0]->iTareaCabGrupoId > 0) {
+
+                $response = ['validated' => true, 'mensaje' => 'Se guardó la información exitosamente.'];
+                $codeResponse = 200;
+            } else {
+                $response = ['validated' => false, 'mensaje' => 'No se ha podido guardar la información.'];
+                $codeResponse = 500;
+            }
+        } catch (\Exception $e) {
+            $response = ['validated' => false, 'message' => $e->getMessage(), 'data' => []];
+            $codeResponse = 500;
+        }
+
+        return new JsonResponse($response, $codeResponse);
+    }
+
+    public function guardarCalificacionTareaCabeceraGruposDocente(Request $request){
+
+        if ($request->iEscalaCalifId) {
+            $iEscalaCalifId = $this->hashids->decode($request->iEscalaCalifId);
+            $iEscalaCalifId = count($iEscalaCalifId) > 0 ? $iEscalaCalifId[0] : $iEscalaCalifId;
+        }
+
+        $parametros = [
+            $request->iTareaCabGrupoId,
+            $iEscalaCalifId,
+            $request->cTareaGrupoComentarioDocente,
+        ];
+
+        try {
+            $data = DB::select('exec aula.SP_UPD_tareaCabeceraGruposCalificarDocente
+                ?,?,?', $parametros);
+
+            if ($data[0]->iTareaCabGrupoId > 0) {
+
+                $response = ['validated' => true, 'mensaje' => 'Se guardó la información exitosamente.'];
+                $codeResponse = 200;
+            } else {
+                $response = ['validated' => false, 'mensaje' => 'No se ha podido guardar la información.'];
+                $codeResponse = 500;
+            }
+        } catch (\Exception $e) {
+            $response = ['validated' => false, 'message' => $e->getMessage(), 'data' => []];
+            $codeResponse = 500;
+        }
+
+        return new JsonResponse($response, $codeResponse);
+    }
+
 }
