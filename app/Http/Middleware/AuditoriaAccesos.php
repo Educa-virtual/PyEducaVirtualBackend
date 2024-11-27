a47f811899856b81b93fb71e02e2230f11f26b16
Andree-R <Andree_Rios_5@hotmail.com>
2024-11-27 17:14:12 -0500

    Fix: Registro de inicio de sesión e intentos fallidos

--- app/Http/Middleware/AuditoriaAccesos.php
+++ app/Http/Middleware/AuditoriaAccesos.php
@@ -6,6 +6,7 @@ use Closure;
 use Jenssegers\Agent\Agent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuditoriaAccesos
@@ -25,60 +26,43 @@ class AuditoriaAccesos
         $agent = new Agent();
 
         if ($response instanceof \Illuminate\Http\JsonResponse) {
-            $originalContent = $response->getData(true); // Obtiene el contenido como array
+            $originalContent = $response->getData(true); 
 
-            // Verifica la presencia de 'user' y modifica la respuesta
-            // if (isset($originalContent['user'])) {
-            //     $userExist = DB::select(
-            //         "EXEC grl.SP_SEL_DesdeTablaOVista ?,?,?,?",
-            //         [
-            //             'seg',
-            //             'credenciales',
-            //             'cCredUsuario',
-            //             'cCredUsuario=' . $request->user,
-            //         ]
-            //     );
+            if (isset($originalContent['user']['iCredId'])) {
+                DB::select(
+                    "EXEC grl.SP_INS_EnTablaDesdeJSON ?,?,?",
+                    [
+                        'seg',
+                        'auditoria_accesos',
+                        json_encode([
+                            'iCredId' => $originalContent['user']['iCredId'],
+                            'cIpCliente' => $this->getIPCliente(),
+                            'cNavegador' => $agent->browser(),
+                            'cDispositivo' => $agent->deviceType(),
+                            'cSistmaOperativo' => $this->getSOCliente(),
+                        ])
+                    ]
+                );
+            }
 
-            //     DB::select(
-            //         "EXEC grl.SP_INS_EnTablaDesdeJSON ?,?,?",
-            //         [
-            //             'seg',
-            //             'auditoria_accesos',
-            //             json_encode([
-            //                 'iCredId' => $originalContent['user']['iCredId'],
-            //                 'cIpCliente' => $this->getIPCliente(),
-            //                 'cNavegador' => $agent->browser(),
-            //                 'cDispositivo' => $agent->deviceType(),
-            //                 'cSistmaOperativo' => $agent->platform(),
-            //             ])
-            //         ]
-            //     );
-            //     $originalContent['extra_info'] = 'Inicio de sesión exitoso';
-            //     $originalContent['userExist'] = $userExist;
-            // }
-
-            // // Verifica la presencia de 'error' y modifica la respuesta
-            // if (isset($originalContent['error'])) {
-            //     DB::select(
-            //         "EXEC grl.SP_INS_EnTablaDesdeJSON ?,?,?",
-            //         [
-            //             'seg',
-            //             'auditoria_accesos_fallidos',
-            //             json_encode([
-            //                 'cLogin' => $request->user,
-            //                 'cPassword' => $request->pass,
-            //                 'cMotivo' => $originalContent['error'],
-            //                 'cIpCliente' => $this->getIPCliente(),
-            //                 'cNavegador' => $agent->browser(),
-            //                 'cDispositivo' => $agent->deviceType(),
-            //                 'cSistmaOperativo' => $agent->platform(),
-            //             ])
-            //         ]
-            //     );
-            //     $originalContent['extra_info'] = 'Error de inicio de sesión';
-            // }
-
-            // Sobrescribir el contenido de la respuesta
+            if (isset($originalContent['error'])) {
+                DB::select(
+                    "EXEC grl.SP_INS_EnTablaDesdeJSON ?,?,?",
+                    [
+                        'seg',
+                        'auditoria_accesos_fallidos',
+                        json_encode([
+                            'cLogin' => $request->user,
+                            'cPassword' => $request->pass,
+                            'cMotivo' => $originalContent['error'],
+                            'cIpCliente' => $this->getIPCliente(),
+                            'cNavegador' => $agent->browser(),
+                            'cDispositivo' => $agent->deviceType(),
+                            'cSistmaOperativo' => $this->getSOCliente(),
+                        ])
+                    ]
+                );
+            }
             $response->setData($originalContent);
         }
 
@@ -107,5 +91,36 @@ class AuditoriaAccesos
         }
 
         return $ipAddress;
+    }
+
+    function getSOCliente() {
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+        $osArray = [
+            'Windows 10' => '/Windows NT 10.0/i',
+            'Windows 8.1' => '/Windows NT 6.3/i',
+            'Windows 8' => '/Windows NT 6.2/i',
+            'Windows 7' => '/Windows NT 6.1/i',
+            'Windows Vista' => '/Windows NT 6.0/i',
+            'Windows Server 2003/XP x64' => '/Windows NT 5.2/i',
+            'Windows XP' => '/Windows NT 5.1/i',
+            'Windows 2000' => '/Windows NT 5.0/i',
+            'Mac OS X' => '/Mac OS X/i',
+            'Mac OS' => '/Mac_PowerPC/i',
+            'Linux' => '/Linux/i',
+            'Ubuntu' => '/Ubuntu/i',
+            'iPhone' => '/iPhone/i',
+            'iPod' => '/iPod/i',
+            'iPad' => '/iPad/i',
+            'Android' => '/Android/i',
+            'BlackBerry' => '/BlackBerry/i',
+            'Mobile' => '/Mobile/i'
+        ];
+
+        foreach ($osArray as $os => $regex) 
+            if (preg_match($regex, $userAgent)) 
+                return $os;
+
+        return 'SO Desconocido';
     }
 }
