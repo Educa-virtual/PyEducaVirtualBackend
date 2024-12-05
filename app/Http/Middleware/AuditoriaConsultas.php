e0c6969be156a028aa3a8a70c581a49f3ce155f9
Andree-R <Andree_Rios_5@hotmail.com>
2024-12-04 23:27:27 -0500

    Fix: Middleware para registrar cambios en las consultas

--- app/Http/Middleware/AuditoriaConsultas.php
+++ app/Http/Middleware/AuditoriaConsultas.php
@@ -19,28 +19,49 @@ class AuditoriaConsultas
   public function handle(Request $request, Closure $next): Response
   {
 
-    $queries = [];
-    // Escuchar todas las consultas
-    DB::listen(function ($query) use (&$queries) {
-      $queries[] = [
-        'sql' => $query->sql,
-        'bindings' => $query->bindings,
-        'time' => $query->time,
-      ];
-    });
+    if ($request->header('iCredId')) {
+      # code...
+      $query = DB::select('EXEC seg.SP_UPD_IniciarAuditoriaBackend');
+
+      // $queries = [];
+      // DB::listen(function ($query) use (&$queries) {
+      //   $queries[] = [
+      //     'sql' => $query->sql,
+      //     'process_id' => DB::select('EXEC seg.SP_UPD_IniciarAuditoriaBackend'),
+      //     'bindings' => $query->bindings,
+      //     'time' => $query->time,
+      //   ];
+      // });
+    }
+
+
     /**
      * @var \Illuminate\Http\JsonResponse $response
      */
     $response = $next($request);
 
+    if ($request->header('iCredId')) {
+      # code...
+      DB::statement('EXEC seg.SP_UPD_DetenerAuditoriaBackend');
 
-    
-
-    $originalContent = $response->getData(true);
-
-    $originalContent['request'] = $queries;
-
-    $response->setData($originalContent);
+      $originalContent = $response->getData(true);
+  
+      DB::select('EXEC grl.SP_UPD_EnTablaConJSON ?,?,?,?', [
+        'seg',
+        'auditorias_middleware',
+        json_encode([
+          'iCredId' => $request->header('iCredId')
+        ]),
+        json_encode([
+          'COLUMN_NAME' => 'proceso_id',
+          'VALUE' => $query[0]->proceso_id,
+        ])
+      ]);
+  
+      // $originalContent['request'] = $queries;
+  
+      $response->setData($originalContent);
+    }
 
     return $response;
   }
