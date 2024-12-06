70573699cd425c501f2cd5937c525e260b228f1a
Mitwar99 <mitwarhuacan@hotmail.com>
2024-12-05 23:37:06 -0500

    funcion obtener logros finales

--- app/Http/Controllers/aula/ResultadoController.php
+++ app/Http/Controllers/aula/ResultadoController.php
@@ -89,42 +89,69 @@ class ResultadoController extends Controller
 
     }
     public function obtenerCalificacionesFinalesReporte(Request $request){
-
-    }
-    public function getDatos(Request $request)
-    {
         // Validar los datos de entrada
         $request->validate([
             'tabla' => 'required|string',
             'campos' => 'nullable|string',
             'where' => 'nullable|string'
         ]);
-        $esquema='dbo';
+        $esquema='acad';
         $tabla = $request->input('tabla');
         $campos = $request->input('campos', '*'); // Usar todos los campos si no se especifica
         $where = $request->input('where', '1=1'); // Condición por defecto si no se proporciona
         //$where = addslashes($request->input('where', '1=1'));
-
-
-        try {
+        try { 
             // Ejecutar el procedimiento almacenado usando DB::select
-            // $results = DB::select("EXEC nombre_sp @table_name = ?, @where_condition = ?", [$tabla, $where]);
-            $results = DB::select('EXEC SP_SEL_DesdeTablaOVista
-                    @nombreEsquema = ?, @nombreObjeto = ?,@campos = ?, @condicionWhere=?',
-                    [$esquema, $tabla,$campos, $where]);
-            // Retornar los datos en formato JSON
-            return response()->json([
-                'status' => 'success',
-               'data' => $results ?: [] // Asegurarse de que 'results' sea siempre un arreglo
-            ]);
-        } catch (\Exception $e) {
-            // Manejar cualquier error y retornar un mensaje adecuado
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Error al ejecutar la consulta: ' . $e->getMessage()
-            ], 500);
+            $data = DB::select('EXEC grl.SP_SEL_DesdeTablaOVista
+                        @nombreEsquema = ?, @nombreObjeto = ?,@campos = ?, @condicionWhere=?',
+                        [$esquema, $tabla,$campos, $where]);
+            
+            $response = ['validated' => true, 'message' => 'se obtuvo la información', 'data' => $data];
+            $estado = 200;
+
+            return $response;
+           
+       }       
+       catch (\Exception $e) {
+        $response = ['validated' => false, 'message' => $e->getMessage(), 'data' => []];
+        $estado = 500;
         }
+        return new JsonResponse($response,$estado);
     }
+    // public function getDatos(Request $request)
+    // {
+    //     // Validar los datos de entrada
+    //     $request->validate([
+    //         'tabla' => 'required|string',
+    //         'campos' => 'nullable|string',
+    //         'where' => 'nullable|string'
+    //     ]);
+    //     $esquema='dbo';
+    //     $tabla = $request->input('tabla');
+    //     $campos = $request->input('campos', '*'); // Usar todos los campos si no se especifica
+    //     $where = $request->input('where', '1=1'); // Condición por defecto si no se proporciona
+    //     //$where = addslashes($request->input('where', '1=1'));
+
+
+    //     try {
+    //         // Ejecutar el procedimiento almacenado usando DB::select
+    //         // $results = DB::select("EXEC nombre_sp @table_name = ?, @where_condition = ?", [$tabla, $where]);
+    //         $results = DB::select('EXEC SP_SEL_DesdeTablaOVista
+    //                 @nombreEsquema = ?, @nombreObjeto = ?,@campos = ?, @condicionWhere=?',
+    //                 [$esquema, $tabla,$campos, $where]);
+    //         // Retornar los datos en formato JSON
+    //         return response()->json([
+    //             'status' => 'success',
+    //            'data' => $results ?: [] // Asegurarse de que 'results' sea siempre un arreglo
+    //         ]);
+    //     } catch (\Exception $e) {
+    //         // Manejar cualquier error y retornar un mensaje adecuado
+    //         return response()->json([
+    //             'status' => 'error',
+    //             'message' => 'Error al ejecutar la consulta: ' . $e->getMessage()
+    //         ], 500);
+    //     }
+    // }
     // public function guardarDatos(Request $request){
 
     //     $esquema='dbo';
