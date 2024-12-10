04e9908370bfeca429425b2e72c80c344de14752
DREW <jhoand60@gmail.com>
2024-12-10 06:12:51 -0500

    fix: rename procedures with CRUD

--- app/Http/Controllers/acad/TipoBibliografiasController.php
+++ app/Http/Controllers/acad/TipoBibliografiasController.php
@@ -64,44 +64,4 @@ class TipoBibliografiasController extends Controller
 
         return new JsonResponse($response, $codeResponse);
     }
-    public function store(Request $request)
-    {
-        $request->validate(
-            [
-                'opcion' => 'required',
-            ],
-            [
-                'opcion.required' => 'Hubo un problema al obtener la acción',
-            ]
-        );
-        if ($request->iTipoBiblioId) {
-            $iTipoBiblioId = $this->hashids->decode($request->iTipoBiblioId);
-            $iTipoBiblioId = count($iTipoBiblioId) > 0 ? $iTipoBiblioId[0] : $iTipoBiblioId;
-        }
-
-        $parametros = [
-            $request->opcion,
-            $request->valorBusqueda ?? '-',
-            $iTipoBiblioId                         ?? NULL,
-            $request->cTipoBiblioNombre            ?? NULL,
-            $request->iCredId
-        ];
-
-        try {
-            $data = DB::select('exec acad.Sp_ACAD_CRUD_TIPO_BIBLIOGRAFIAS
-                ?,?,?,?,?', $parametros);
-
-            foreach ($data as $key => $value) {
-                $value->iTipoBiblioId = $this->hashids->encode($value->iTipoBiblioId);
-            }
-
-            $response = ['validated' => true, 'message' => 'se obtuvo la información', 'data' => $data];
-            $codeResponse = 200;
-        } catch (Exception $e) {
-            $response = ['validated' => false, 'message' => $e->getMessage(), 'data' => []];
-            $codeResponse = 500;
-        }
-
-        return new JsonResponse($response, $codeResponse);
-    }
 }
