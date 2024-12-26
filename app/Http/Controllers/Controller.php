e6051d6c01c119cbe2f7f9fc57cc6465c3c78552
Andree-R <Andree_Rios_5@hotmail.com>
2024-12-26 14:50:54 -0500

    Add: Procedures basics

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Exception;
 use App\Helpers\ResponseHandler;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Collection;
@@ -20,11 +21,11 @@ abstract class Controller extends BaseController
      * Este método permite ejecutar un procedimiento almacenado para consultar 
      * datos desde una tabla o vista, aplicando filtros opcionales.
      * 
-     * @param string $esquema Nombre del esquema de la base de datos.
-     * @param string $tablaOVista Nombre de la tabla o vista a consultar.
-     * @param string $campos Campos a seleccionar, separados por comas (por 
+     * @param string $request->esquema Nombre del esquema de la base de datos.
+     * @param string $request->tablaOVista Nombre de la tabla o vista a consultar.
+     * @param string $request->campos Campos a seleccionar, separados por comas (por 
      * defecto '*').
-     * @param string|null $condicionWhere Condición WHERE para filtrar los 
+     * @param string|null $request->condicionWhere Condición WHERE para filtrar los 
      * resultados (opcional).
      * @return Collection|ResponseHandler
      * 
@@ -46,12 +47,12 @@ abstract class Controller extends BaseController
      * ```
      */
 
-    public function selDesdeTablaOVista($esquema, $tablaOVista, $campos = '*', $condicionWhere = null):Collection|JsonResponse
+    public function selDesdeTablaOVista(Request $request): Collection|JsonResponse
     {
-        $params = [$esquema, $tablaOVista, $campos];
+        $params = [$request->esquema, $request->tabla, $request->data ?? '*'];
 
-        if (!is_null($condicionWhere)) {
-            $params[] = $condicionWhere;
+        if (!is_null($request->condicionWhere)) {
+            $params[] = $request->condicionWhere;
         }
 
         // Construir los placeholders dinámicos
@@ -62,6 +63,112 @@ abstract class Controller extends BaseController
             return collect(DB::select("EXEC grl.SP_SEL_DesdeTablaOVista $placeholders", $params));
         } catch (Exception $e) {
             return ResponseHandler::error('Error al obtener los datos.', 500, $e->getMessage());
+        }
+    }
+
+
+    public function insEnTablaDesdeJSON(Request $request): Collection|JsonResponse
+    {
+        if (is_null($request->data)) {
+            return ResponseHandler::error('Error sin datos que insertar.', 500);
+        }
+
+        $insertsIds = [];
+
+        if (is_array($request->data)) {
+
+            foreach ($request->data as $campos) {
+                $params = [$request->esquema, $request->tabla];
+
+                $params[] = json_encode($campos);
+
+                $placeholders = implode(',', array_fill(0, count($params), '?'));
+
+                try {
+                    $insertIds[] = DB::select("EXEC grl.SP_INS_EnTablaDesdeJSON $placeholders", $params);
+                } catch (Exception $e) {
+                    return ResponseHandler::error('Error al insertar los datos.', 500, $e->getMessage());
+                }
+            }
+
+            return collect($insertIds);
+        } else {
+            return ResponseHandler::error('Error al insertar los datos.');
+        }
+    }
+
+    public function updEnTablaConJSON(Request $request): Collection|JsonResponse
+    {
+        if (is_null($request->data)) {
+            return ResponseHandler::error('Error campos no especificados', 500);
+        }
+
+        if (is_array($request->data)) {
+
+            foreach ($request->data as $registro) {
+
+                foreach ($registro as $key => $value) {
+
+                    $params = [$request->esquema, $request->tabla];
+
+                    if (!is_null($registro['campos'])) {
+                        $params[] = json_encode($registro['campos']);
+                    }
+                    if (!is_null($registro['where'])) {
+                        $params[] = json_encode($registro['where']);
+                    }
+
+                    $placeholders = implode(',', array_fill(0, count($params), '?'));
+
+                    try {
+                        $updateIds[] = DB::select("EXEC grl.SP_UPD_EnTablaConJSON $placeholders", $params);
+                    } catch (Exception $e) {
+                        return ResponseHandler::error('Error al actualizar los datos.', 500, $e->getMessage());
+                    }
+                }
+            }
+
+            return collect($updateIds);
+        } else {
+            return ResponseHandler::error('Error al actualizar los datos.');
+        }
+    }
+    public function delRegistroConTransaccion(Request $request): Collection|JsonResponse
+    {
+
+        if (is_null($request->campoId)) {
+            return ResponseHandler::error('Error campos no especificados', 500);
+        }
+
+        if (is_array($request->data)) {
+
+            foreach ($request->data as $registro) {
+
+                foreach ($registro as $key => $value) {
+
+                    $params = [$request->esquema, $request->tabla, $request->campoId];
+
+                    if (isset($registro['valorId']) AND !is_null($registro['valorId'])) {
+                        $params[] = $registro['valorId'];
+                    }
+
+                    if (isset($registro['tablaHija']) AND !is_null($registro['tablaHija']) AND $registro['tablaHija'] != NULL ) {
+                        $params[] = $registro['tablaHija'];
+                    }
+
+                    $placeholders = implode(',', array_fill(0, count($params), '?'));
+
+                    try {
+                        $updateIds[] = DB::select("EXEC grl.SP_DEL_RegistroConTransaccion $placeholders", $params);
+                    } catch (Exception $e) {
+                        return ResponseHandler::error('Error al borrar los datos.', 500, $e->getMessage());
+                    }
+                }
+            }
+
+            return collect($updateIds);
+        } else {
+            return ResponseHandler::error('Error al borrar los datos.');
         }
     }
 }
