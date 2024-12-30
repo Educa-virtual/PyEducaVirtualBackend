bb1f117c1dccd5ca94f36bade9e465266a16e769
DREW <jhoand60@gmail.com>
2024-12-30 08:36:08 -0500

    feat(GradosController): new

--- routes/acad/api.php
+++ routes/acad/api.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\acad\EstudiantesController;
+use App\Http\Controllers\acad\GradosController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'acad'], function () {
 
     Route::group(['prefix' => 'estudiantes'], function () {
         Route::post('obtenerCursosXEstudianteAnioSemestre', [EstudiantesController::class, 'obtenerCursosXEstudianteAnioSemestre']);
+    });
+    Route::group(['prefix' => 'grados'], function () {
+        Route::post('handleCrudOperation', [GradosController::class, 'handleCrudOperation']);
     });
 });
