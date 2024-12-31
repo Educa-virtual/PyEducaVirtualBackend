a2ea16f5e5730cd4ea06b9a6bd4fa500be8a1909
Andree-R <Andree_Rios_5@hotmail.com>
2024-12-31 09:00:38 -0500

    Fix: obtener rubrica

--- routes/evaluaciones/api.php
+++ routes/evaluaciones/api.php
@@ -59,7 +59,7 @@ Route::group(['prefix' => 'evaluaciones',], function () {
         Route::resource('rubrica', InstrumentosEvaluacionController::class);
         Route::get('obtenerRubricas', [InstrumentosEvaluacionController::class, 'obtenerRubricas']);
         Route::get('obtenerRubrica', [InstrumentosEvaluacionController::class, 'obtenerRubrica']);
-        Route::get('obtenerRubricaEvaluacion', [InstrumentosEvaluacionController::class, 'obtenerRubricaEvaluacion']);
+        Route::post('obtenerRubricaEvaluacion', [InstrumentosEvaluacionController::class, 'obtenerRubricaEvaluacion']);
     });
 
     Route::resource('escala-calificaciones', EscalaCalificacionesController::class);
