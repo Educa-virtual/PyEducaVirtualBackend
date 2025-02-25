5c95f83ab7d8d64f79a1b8177995017be3576588
Wilmar Garibaldi <w_garibaldi@hotmail.com>
2025-02-25 07:47:38 -0500

    Mejoras en las URL para especialistas dremo

--- routes/acad/api.php
+++ routes/acad/api.php
@@ -14,13 +14,14 @@ Route::group(['prefix' => 'acad'], function () {
     Route::group(['prefix' => 'grados'], function () {
         Route::post('handleCrudOperation', [GradosController::class, 'handleCrudOperation']);
     });
+    Route::group(['prefix' => 'especialistas-dremo'], function () {
+        Route::get('', [EspecialistasDremoController::class, 'obtenerEspecialistas']);
+        Route::get('{docenteId}/areas', [EspecialistasDremoController::class, 'obtenerAreasPorEspecialista']);
+        Route::post('{docenteId}/areas', [EspecialistasDremoController::class, 'asignarAreaEspecialista']);
+        Route::delete('{docenteId}/areas', [EspecialistasDremoController::class, 'eliminarAreaEspecialista']);
+    });
     Route::group(['prefix' => 'docentes'], function () {
-        Route::group(['prefix' => 'especialistas-dremo'], function () {
-            Route::get('', [EspecialistasDremoController::class, 'obtenerEspecialistas']);
-            Route::get('{docenteId}/areas', [EspecialistasDremoController::class, 'obtenerAreasPorEspecialista']);
-            Route::post('{docenteId}/areas', [EspecialistasDremoController::class, 'asignarAreaEspecialista']);
-            Route::delete('{docenteId}/areas', [EspecialistasDremoController::class, 'eliminarAreaEspecialista']);
-        });
+
     });
     Route::group(['prefix' => 'cursos'], function () {
         Route::get('', [CursosController::class, 'listarCursosPorNivel']);
