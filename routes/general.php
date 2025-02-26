cad13fb1fa95e0637f101d11d32c64fc020ad189
Andree-R <Andree_Rios_5@hotmail.com>
2025-02-26 16:53:31 -0500

    feat(FileController): agregar funcionalidades de carga y descarga de archivos

--- routes/general.php
+++ routes/general.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\FileController;
 use App\Http\Controllers\grl\GeneralController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'general'], function () {
     Route::post('subir-archivo', [GeneralController::class, 'subirArchivo']);
+});
+
+Route::group(['prefix' => 'file'], function () {
+    Route::get('import', [FileController::class, 'downloadFile']);
 });
