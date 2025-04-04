6bf969be1d1b2858e5473a6d7dd7293963fc9c82
DREW <jhoand60@gmail.com>
2025-04-04 15:59:37 -0500

    feat: Add controllers and routes for TipoCapacitaciones, NivelPedagogicos, and TipoPublicos

---
+++ app/Http/Controllers/acad/CapacitacionesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\acad;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CapacitacionesController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/acad/TipoPublicosController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\acad;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use App\Helpers\VerifyHash;
+use Illuminate\Http\Response;
+
+class TipoPublicosController extends Controller
+{
+    public function listarTipoPublicos()
+    {
+        try {
+            $fieldsToDecode = [
+                'iTipoPubId',
+            ];
+            
+            $data = DB::select(
+                'exec acad.SP_SEL_tipoPublicos',
+            );
+            $data = VerifyHash::encodeRequest($data, $fieldsToDecode);
+            return new JsonResponse(
+                ['validated' => true, 'message' => 'Se ha obtenido exitosamente ', 'data' => ($data)],
+                Response::HTTP_OK
+            );
+        } catch (\Exception $e) {
+            return new JsonResponse(
+                ['validated' => false, 'message' => substr($e->errorInfo[2] ?? '', 54), 'data' => []],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
