d430fd4dc9e3053336c16a30eda5e91c74791d4c
Wilmar Garibaldi <w_garibaldi@hotmail.com>
2025-05-21 09:51:24 -0500

    Corrección de fecha de evaluación

--- app/Http/Controllers/Ere/EvaluacionesController.php
+++ app/Http/Controllers/Ere/EvaluacionesController.php
@@ -222,8 +222,8 @@ class EvaluacionesController extends ApiController
             'cEvaluacionNombre' => $request->input('cEvaluacionNombre', null),
             'cEvaluacionDescripcion' => $request->input('cEvaluacionDescripcion', null),
             'cEvaluacionUrlDrive' => $request->input('cEvaluacionUrlDrive', null),
-            'dtEvaluacionFechaInicio' => $request->input('dtEvaluacionFechaInicio', null),
-            'dtEvaluacionFechaFin' => $request->input('dtEvaluacionFechaFin', null),
+            'dtEvaluacionFechaInicio' => Carbon::parse($request->input('dtEvaluacionFechaInicio'))->toDateTimeString(),
+            'dtEvaluacionFechaFin' => Carbon::parse($request->input('dtEvaluacionFechaFin'))->toDateTimeString()//$request->input('dtEvaluacionFechaFin', null),
         ];
 
         // return $params;
