c761440ecd02afbaad93e10eca2b6d8aad4844af
DREW <jhoand60@gmail.com>
2025-05-27 10:11:42 -0500

    feat(CuestionariosController): enhance actualizarCuestionario and eliminarCuestionario methods; add validation and database interaction

--- app/Http/Controllers/aula/CuestionariosController.php
+++ app/Http/Controllers/aula/CuestionariosController.php
@@ -99,9 +99,12 @@ class CuestionariosController extends Controller
         }
     }
 
-    public function actualizarCuestionario(Request $request, $id)
-    {    // Validación de los parámetros de entrada
+    public function actualizarCuestionario(Request $request, $iCuestionarioId)
+    {    
+        $request->merge(['iCuestionarioId' => $iCuestionarioId]);
+        // Validación de los parámetros de entrada
         $validator = Validator::make($request->all(), [
+            'iCuestionarioId' => ['required'],
             'iProgActId' => ['required'],
             'iDocenteId' => ['required'],
             'cTitulo' => ['required', 'max:250'],
@@ -110,6 +113,7 @@ class CuestionariosController extends Controller
             'dtInicio'     => ['required', 'date'],
             'dtFin'        => ['required', 'date', 'after:dtInicio'],
         ], [
+            'iCuestionarioId.required' => 'No se encontro el identificador iCuestionarioId',
             'iProgActId.required' => 'No se encontro el identificador iProgActId',
             'iDocenteId.required' => 'No se encontro el identificador iDocenteId',
             'cTitulo.required' => 'No se encontro el identificador cTitulo',
@@ -130,12 +134,114 @@ class CuestionariosController extends Controller
             ], Response::HTTP_UNPROCESSABLE_ENTITY);
         }
         
-        // Implementar la lógica para actualizar un cuestionario
-        return response()->json(['message' => 'Cuestionario actualizado correctamente'], 200);
+       try {
+            $fieldsToDecode = [
+                'iProgActId',
+                'iDocenteId',
+                'iCredId',
+            ];
+            $request =  VerifyHash::validateRequest($request, $fieldsToDecode);
+
+            $parametros = [
+                $request->iCuestionarioId   ?? NULL,
+                $request->iProgActId        ?? NULL,
+                $request->iDocenteId        ?? NULL,
+                $request->cTitulo           ?? NULL,
+                $request->cSubtitulo        ?? NULL,
+                $request->cDescripcion      ?? NULL,
+                $request->dtInicio          ?? NULL,
+                $request->dtFin             ?? NULL,
+                $request->cArchivoAdjunto   ?? NULL,
+
+                $request->iCredId           ?? NULL
+            ];
+
+            $data = DB::select(
+                'exec aula.SP_UPD_cuestionarios 
+                    @_iCuestionarioId=?,
+                    @_iProgActId=?,
+                    @_iDocenteId=?,
+                    @_cTitulo=?,
+                    @_cSubtitulo=?,
+                    @_cDescripcion=?,
+                    @_dtInicio=?,
+                    @_dtFin=?,
+                    @_cArchivoAdjunto=?,
+                    @_iCredId=?',
+                $parametros
+            );
+
+            if ($data[0]->iCuestionarioId > 0) {
+                return new JsonResponse(
+                    ['validated' => true, 'message' => 'Se ha actualizado exitosamente ', 'data' => null],
+                    Response::HTTP_OK
+                );
+            } else {
+                return new JsonResponse(
+                    ['validated' => false, 'message' => 'No se ha podido actualizar', 'data' => null],
+                    Response::HTTP_OK
+                );
+            }
+        } catch (Exception $e) {
+            return new JsonResponse(
+                ['validated' => false, 'message' => substr($e->errorInfo[2] ?? '', 54), 'data' => []],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
-    public function eliminarCuestionario($id)
+    public function eliminarCuestionario(Request $request,$iCuestionarioId)
     {
-        // Implementar la lógica para eliminar un cuestionario
-        return response()->json(['message' => 'Cuestionario eliminado correctamente'], 200);
+        $request->merge(['iCuestionarioId' => $iCuestionarioId]);
+        // Validación de los parámetros de entrada
+        $validator = Validator::make($request->all(), [
+            'iCuestionarioId' => ['required'],
+        ], [
+            'iCuestionarioId.required' => 'No se encontro el identificador iCuestionarioId',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'validated' => false,
+                'errors' => $validator->errors()
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try {
+            $fieldsToDecode = [
+                'iCuestionarioId',
+                'iCredId',
+            ];
+            
+            $request =  VerifyHash::validateRequest($request, $fieldsToDecode);
+
+            $parametros = [
+                $request->iCuestionarioId      ??  NULL,
+                $request->iCredId              ??  NULL
+            ];
+
+            $data = DB::select(
+                'exec aula.SP_DEL_cuestionariosxiCuestionarioId
+                    @_iCuestionarioId=?, 
+                    @_iCredId=?',
+                $parametros
+            );
+
+            if ($data[0]->iCuestionarioId > 0) {
+                return new JsonResponse(
+                    ['validated' => true, 'message' => 'Se ha eliminado exitosamente ', 'data' => null],
+                    Response::HTTP_OK
+                );
+            } else {
+                return new JsonResponse(
+                    ['validated' => false, 'message' => 'No se ha podido eliminar', 'data' => null],
+                    Response::HTTP_OK
+                );
+            }
+        } catch (\Exception $e) {
+            return new JsonResponse(
+                ['validated' => false, 'message' => substr($e->errorInfo[2] ?? '', 54), 'data' => []],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 }
