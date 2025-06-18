7543c86a8e836e71d4db179c1c3804fc844c4395
DREW <jhoand60@gmail.com>
2025-06-18 14:32:49 -0500

    refactor: remove unused fieldsToDecode in listarPreguntasxiCuestionarioId method

--- app/Http/Controllers/aula/PreguntasController.php
+++ app/Http/Controllers/aula/PreguntasController.php
@@ -36,11 +36,7 @@ class PreguntasController extends Controller
                     @_iCredId=?',
                 $parametros
             );
-            $fieldsToDecode = [
-                'iCuestionarioId',
-                'iPregId',
-                'iCredId',
-            ];
+
             $data = VerifyHash::encodeRequest($data, $fieldsToDecode);
 
             foreach ($data as $pregunta) {
