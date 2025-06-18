1a5342b446d527009c0646d794b4c99465ab525b
carlos alberto chura flores <chura.f5@gmail.com>
2025-06-18 07:43:43 -0500

    Se modifico en el models parametros

--- app/Models/asi/AsistenciaAdministrativa.php
+++ app/Models/asi/AsistenciaAdministrativa.php
@@ -10,7 +10,7 @@ class AsistenciaAdministrativa extends Model
 {
     public static function buscarHorarioInstitucion(Request $request){
         $datos = [
-            $request["iSedeId"],
+            $request->iSedeId,
         ];
         $data = DB::select("EXEC [asi].[SP_SEL_configuracion_horario] ?",$datos);
         return $data;
