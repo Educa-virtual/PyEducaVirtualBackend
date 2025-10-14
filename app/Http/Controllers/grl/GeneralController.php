a9cb1485ce9f5ca2ab9c0fff884f196b1e43eab1
carlos alberto chura flores <chura.f5@gmail.com>
2025-10-14 13:56:03 -0500

    Se modifico ruta de almacenamiento de archivos

--- app/Http/Controllers/grl/GeneralController.php
+++ app/Http/Controllers/grl/GeneralController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\grl;
 
+use App\Helpers\VerifyHash;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -29,9 +30,19 @@ class GeneralController extends Controller
 
         if ($request->hasFile('file')) {
 
-            $file = $request->file('file');
-            $path = $request->file("file")->store($request->nameFile, ['disk' => 'file']);
-            return new JsonResponse(['validated' => true, 'message' => 'Se guardó exitosamente el archivo', 'data' => $path], 200);
+            $iPersId = VerifyHash::decodes($request->iPersId);
+            $documento = $request->file('file');
+            $tipoPortafolio = $request->tipoPortafolio;
+            $cIieeCodigoModular = $request->cIieeCodigoModular;
+            $years = $request->years;
+            $carpeta = '';
+            $folder = $years.'/'.$cIieeCodigoModular.'/'.$iPersId.'/'.$carpeta;      
+            $generado = Storage::disk('public')->putFile($folder,$documento);
+            $ruta = $folder.'/'.basename($generado);
+
+            // $file = $request->file('file');
+            // $path = $request->file("file")->store($request->nameFile, ['disk' => 'file']);
+            return new JsonResponse(['validated' => true, 'message' => 'Se guardó exitosamente el archivo', 'data' => $ruta], 200);
             return response()->json($path);
         } else {
             return new JsonResponse(['validated' => false, 'message' => 'No se adjuntaron archivos', 'data' => []], 503);
