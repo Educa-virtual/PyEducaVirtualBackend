20de7b242a40397299e37554bb7480ed43c6e3b9
Wilmar Garibaldi <w_garibaldi@hotmail.com>
2025-10-20 10:00:22 -0500

    Importacion de apoderados de estudiantes

--- app/Services/apo/ApoderadosService.php
+++ app/Services/apo/ApoderadosService.php
@@ -4,7 +4,12 @@ namespace App\Services\apo;
 
 use App\Helpers\VerifyHash;
 use App\Models\apo\Apoderado;
+use App\Services\acad\InstitucionesEducativasService;
+use App\Services\FormatearExcelPadresService;
+use App\Services\LeerExcelService;
+use App\Services\seg\UsuariosService;
 use Exception;
+use Illuminate\Http\Request;
 
 class ApoderadosService
 {
@@ -12,15 +17,31 @@ class ApoderadosService
     {
         $data = Apoderado::selEstudiantesPorApoderado($iPersId);
         foreach ($data as $fila) {
-            $fila->iEstudianteId=VerifyHash::encodexId($fila->iEstudianteId);
+            $fila->iEstudianteId = VerifyHash::encodexId($fila->iEstudianteId);
         }
         return $data;
     }
 
-    public static function estudiantePerteneceApoderado($iPersIdApoderado, $iEstudianteId) {
+    public static function estudiantePerteneceApoderado($iPersIdApoderado, $iEstudianteId)
+    {
         $data = Apoderado::selEstudianteApoderado($iPersIdApoderado, $iEstudianteId);
         if (!$data) {
             throw new Exception("El estudiante no esta relacionado con el apoderado");
         }
+    }
+
+    public static function importarDesdeArchivoExcel(Request $request, $iPersId)
+    {
+        $dataExcel = LeerExcelService::leer($request);
+        $dataFormateada = FormatearExcelPadresService::formatear($dataExcel);
+
+        $detallesCredencial = UsuariosService::obtenerDetallesCredencialEntidad($request->header('iCredEntPerfId'));
+        $institucionEducativa = InstitucionesEducativasService::obtenerIePorSede($detallesCredencial->iSedeId);
+        if ($institucionEducativa->cIieeCodigoModular!=$dataFormateada['codigo_modular']) {
+            throw new Exception("El código modular del archivo no coincide con la institución educativa del usuario.");
+        }
+        //$json_estudiantes = str_replace("'", "''", json_encode($dataFormateada['estudiantes']));
+        Apoderado::insApoderadosDesdeArchivo($dataFormateada['estudiantes'], $iPersId);
+
     }
 }
