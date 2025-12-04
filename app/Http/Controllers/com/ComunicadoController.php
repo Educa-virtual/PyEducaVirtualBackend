8d2ba4861f16cb32afae379dc5715517af1bee85
carlos alberto chura flores <chura.f5@gmail.com>
2025-12-04 15:27:45 -0500

    Falta agregar recepcion de comunicados

--- app/Http/Controllers/com/ComunicadoController.php
+++ app/Http/Controllers/com/ComunicadoController.php
@@ -138,4 +138,14 @@ class ComunicadoController extends Controller
             return FormatearMensajeHelper::error($e);
         }
     }
+    public function recepcionarComunicado(Request $request)
+    {
+        try {
+            Gate::authorize('tiene-perfil', [$this->emisores]);
+            $data = Comunicado::insRecepcionarComunicado($request);
+            return FormatearMensajeHelper::ok('Se obtuvo los datos', $data);
+        } catch (Exception $e) {
+            return FormatearMensajeHelper::error($e);
+        }
+    }
 }
